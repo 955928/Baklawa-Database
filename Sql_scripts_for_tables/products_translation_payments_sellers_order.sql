1876074cb2400ea55b713f72b3ec112e3b4e38bf
LaGuize <guilhem.dolet@gmail.com>
2024-01-12 11:48:51 +0100

    Save sur script P_T_P_S_O

---
+++ Sql_scripts_for_tables/products_translation_payments_sellers_order.sql
@@ -0,0 +1,47 @@
+CREATE TABLE IF NOT EXISTS Products(
+    product_id TEXT PRIMARY KEY,
+    product_category_name TEXT,
+    product_name_length INTEGER,
+    product_description_length TEXT,
+    product_photos_qty INTEGER,
+    product_weight_g TEXT,
+    product_length_cm INTEGER,
+    product_height_cm INTEGER,
+    product_width_com INTEGER
+
+);
+
+CREATE TABLE IF NOT EXISTS Payments(
+    order_id TEXT PRIMARY KEY,
+    payment_sequential INTEGER,
+    payment_type TEXT,
+    payment_installments INTEGER,
+    payment_value FLOAT
+
+);
+
+CREATE TABLE IF NOT EXISTS P_Name_Translation(
+    id_products_category_name TEXT PRIMARY KEY,
+    products_category_name TEXT,
+    products_category_name TEXT
+
+);
+
+CREATE TABLE IF NOT EXISTS Sellers(
+    id_sellers TEXT PRIMARY KEY,
+    seller_zip_code INTEGER,
+    seller_city TEXT,
+    seller_State TEXT
+
+);
+
+CREATE TABLE IF NOT EXISTS Products(
+    order_id TEXT PRIMARY KEY,
+    order_purchased_time TEXT,
+    order_approved_time TEXT,
+    order_delivered_carrier TEXT,
+    order_delivered_customer TEXT,
+    order_estimate_delivery TEXT
+    
+
+);
